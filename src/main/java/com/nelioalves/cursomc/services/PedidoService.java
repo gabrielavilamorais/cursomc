ec96f95168d1d631403a86bc382cd2bd797763d8
Gabriel Ávila <gabriel.avila@siriussoftware.com.br>
2022-05-04 16:35:58 -0300

    Endpoint/pedidos/{id} disponibilizado

---
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -0,0 +1,27 @@
+package com.nelioalves.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Pedido;
+import com.nelioalves.cursomc.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Pedido.class.getName());
+		}
+		return obj.isPresent() ? obj.get() : null;
+	}
+
+}
+
